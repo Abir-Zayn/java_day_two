88a3838c9c98b1fb1961a25b88be952106c86abf
Abir-Zayn <62856303+Abir-Zayn@users.noreply.github.com>
2021-12-15 03:06:30 +0600

    Update recursivceFact.java

--- recursivceFact.java
+++ recursivceFact.java
@@ -2,13 +2,16 @@ package RecursiveMethodFactorial;
 
 import java.util.Scanner;
 
-
+//Recursion method been created in this class
 class recursion{
+    
+    //recursive method
     public int Rtest(int n){
 
         if(n == 0 || n==1){
             return 1;
         }
+        // recursive :- method call itself within itself.
         return n * Rtest(n - 1);
     }
 
@@ -16,9 +19,12 @@ class recursion{
 
 public class recursivceFact {
     public static void main(String[] args) {
+        //object created
         recursion re = new recursion();
         System.out.println("Enter the number to calculate its factorial:: ");
         Scanner sc = new Scanner(System.in);
+        // enter numbers to get its factorial numbers
+        
         int n=sc.nextInt();
         System.out.println("The factorial of "+n+" "+"is "+ re.Rtest(n) );
     }
